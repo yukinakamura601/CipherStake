52c3e2fcdea370da9600e14e2fa20d855f005097
yukinakamura601 <htgemv48651@outlook.com>
2025-10-29 18:00:24 +0800

    create erc7984 cUSDT token

---
+++ contracts/cUSDT.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.27;
+
+import {ERC7984} from "@openzeppelin/confidential-contracts/token/ERC7984/ERC7984.sol";
+import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
+import {FHE, euint64} from "@fhevm/solidity/lib/FHE.sol";
+
+contract cUSDT is ERC7984, SepoliaConfig {
+    constructor() ERC7984("cUSDT", "cUSDT", "") {}
+
+    function mint(address to, uint64 amount) public {
+        euint64 encryptedAmount = FHE.asEuint64(amount);
+        _mint(to, encryptedAmount);
+    }
+}
