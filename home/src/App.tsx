7b80af9903a1847fa473bca1c7c5016837d1b7ba
leo-nguyen-sketch <bvbhtg5815@outlook.com>
2025-10-30 16:35:24 +0800

    inti react tempalte

---
+++ home/src/App.tsx
@@ -0,0 +1,25 @@
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { config } from './config/wagmi';
+import { StakeApp } from './components/StakeApp';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider locale="en">
+          <div style={{ minHeight: '100vh', backgroundColor: '#f3f4f6' }}>
+            <StakeApp />
+          </div>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default App
